28b9b3dbb50d968678c4922ebee2572d2d6787bc
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-31 22:36:05 +0100

    Fixed

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,5 +1,6 @@
-use std::io;
+use std::{io, thread};
 use std::io::Write;
+use std::time::Duration;
 
 pub fn delete_last_line() {
     print!("\x1b[1A");
@@ -9,9 +10,16 @@ pub fn delete_last_line() {
 
 pub fn start_counter() {
     println!("            Starting in 3");
+    thread::sleep(Duration::from_secs(1));
     delete_last_line();
+
     println!("            Starting in 2");
+    thread::sleep(Duration::from_secs(1));
     delete_last_line();
+
     println!("            Starting in 1");
+    thread::sleep(Duration::from_secs(1));
     delete_last_line();
+
+    thread::sleep(Duration::from_secs(1));
 }
