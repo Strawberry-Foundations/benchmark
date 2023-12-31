b76db269904dafb72bf237c8756323bcf8cc1188
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-31 22:35:32 +0100

    code structure

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,14 @@
-use std::{env, thread};
-use std::time::Duration;
+#[allow(dead_code)]
+mod colors;
+mod cli_wins;
+mod benchmark;
+mod utilities;
 
-fn benchmark(time: u64) {
-    let running = std::sync::Arc::new(std::sync::atomic::AtomicBool::new(true));
-    let running_clone = running.clone();
+use std::env;
 
-    let mut x: u64 = 0;
-
-    let bench_time = time;
-
-    thread::spawn(move || {
-        thread::sleep(Duration::from_secs(bench_time));
-        running_clone.store(false, std::sync::atomic::Ordering::Relaxed);
-    });
-
-    while running.load(std::sync::atomic::Ordering::Relaxed) {
-        x += 1;
-    }
-
-    let bench_time_ms = bench_time * 1000;
-    let result = (x + bench_time_ms) / 900000;
-
-    println!("Eingestelle Benchmark-Zeit: {}s ({}ms)", bench_time, bench_time_ms);
-    println!("Der Counter hat bis {} gezählt", x);
-    println!("Ergebnis: {result}");
-}
+pub const VERSION: &str = "1.0.0";
 
 fn main() {
-    println!("-- Boolean's Benchmarking Tool v1.0.0 --");
-
     let args: Vec<String> = env::args().collect();
     let bench_time: u64 = if args.len() < 2 {
         10
@@ -37,5 +17,9 @@ fn main() {
         args[1].parse().unwrap()
     };
 
-    benchmark(bench_time);
+    cli_wins::startup(&bench_time);
+
+    utilities::start_counter();
+
+    benchmark::benchmark(bench_time);
 }
