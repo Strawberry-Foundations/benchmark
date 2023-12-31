e0d92f0855089baeea57b7bf1324ab5ec49c9c1d
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-31 22:32:05 +0100

    moved benchmark function to benchmark.rs

---
+++ src/benchmark.rs
@@ -0,0 +1,27 @@
+use std::thread;
+use std::time::Duration;
+
+fn benchmark(time: u64) {
+    let running = std::sync::Arc::new(std::sync::atomic::AtomicBool::new(true));
+    let running_clone = running.clone();
+
+    let mut x: u64 = 0;
+
+    let bench_time = time;
+
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(bench_time));
+        running_clone.store(false, std::sync::atomic::Ordering::Relaxed);
+    });
+
+    while running.load(std::sync::atomic::Ordering::Relaxed) {
+        x += 1;
+    }
+
+    let bench_time_ms = bench_time * 1000;
+    let result = (x + bench_time_ms) / 900000;
+
+    println!("Eingestelle Benchmark-Zeit: {}s ({}ms)", bench_time, bench_time_ms);
+    println!("Der Counter hat bis {} gezählt", x);
+    println!("Ergebnis: {result}");
+}
