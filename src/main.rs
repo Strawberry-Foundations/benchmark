afe0fd6a6bc8002d776f6901c4aa4e66f733f56b
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-31 23:38:56 +0100

    Code Improvements

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,7 @@ pub const VERSION: &str = "1.1.0";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let bench_time: u64 = if args.len() < 2 {
-        10
-    }
-    else {
-        args[1].parse().unwrap()
-    };
+    let bench_time = args.get(1).map(|n| n.parse().unwrap()).unwrap_or(10);
 
     cli_wins::startup(&bench_time);
 
