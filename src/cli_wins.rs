98673292be291599341d7ae81a4b9101fb227874
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-31 23:09:34 +0100

    some fixes

--- src/cli_wins.rs
+++ src/cli_wins.rs
@@ -16,7 +16,7 @@ pub fn startup(bench_time: &u64) {
     println!("\n{BOLD}  {GREEN}* ---- Benchmarking Tool v{VERSION} ---- *{RESET}{C_RESET}");
     return_bench_time(bench_time);
     println!("{BOLD}  {GREEN}|{RESET}    This tool will only use one     {GREEN}|{RESET}{C_RESET}");
-    println!("{BOLD}  {GREEN}|{RESET}            CPU core!               {GREEN}|{RESET}{C_RESET}");
+    println!("{BOLD}  {GREEN}|{RESET}              CPU core!             {GREEN}|{RESET}{C_RESET}");
     println!("{BOLD}  {GREEN}* ---------------------------------- *{GREEN}{C_RESET}\n");
 }
 
@@ -24,7 +24,7 @@ pub fn return_result(bench_time: &u64) {
     match bench_time.to_string().len() {
         1 => println!("{BOLD}  {GREEN}|{RESET}                  {CYAN}{bench_time}                 {GREEN}|{RESET}{C_RESET}"),
         2 => println!("{BOLD}  {GREEN}|{RESET}                 {CYAN}{bench_time}                 {GREEN}|{RESET}{C_RESET}"),
-        3 => println!("{BOLD}  {GREEN}|{RESET}                {CYAN}{bench_time}                {GREEN}|{RESET}{C_RESET}"),
+        3 => println!("{BOLD}  {GREEN}|{RESET}                {CYAN}{bench_time}                 {GREEN}|{RESET}{C_RESET}"),
         4 => println!("{BOLD}  {GREEN}|{RESET}                {CYAN}{bench_time}                {GREEN}|{RESET}{C_RESET}"),
         5 => println!("{BOLD}  {GREEN}|{RESET}                {CYAN}{bench_time}                {GREEN}|{RESET}{C_RESET}"),
         6 => println!("{BOLD}  {GREEN}|{RESET}                {CYAN}{bench_time}                {GREEN}|{RESET}{C_RESET}"),
@@ -37,7 +37,6 @@ pub fn result(result: u64) {
     println!("{BOLD}  {GREEN}|{RESET}      Your computer has scored      {GREEN}|{RESET}{C_RESET}");
     return_result(&result);
     println!("{BOLD}  {GREEN}|{RESET}               points!              {GREEN}|{RESET}{C_RESET}");
-    println!("{BOLD}  {GREEN}|{RESET}            cpu core!               {GREEN}|{RESET}{C_RESET}");
     println!("{BOLD}  {GREEN}* ---------------------------------- *{GREEN}{C_RESET}\n");
 }
 
