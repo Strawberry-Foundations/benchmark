a44ce2de66d58b34f09504067eb23939df8bcf99
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-31 22:57:35 +0100

    some major updates

--- src/cli_wins.rs
+++ src/cli_wins.rs
@@ -20,3 +20,24 @@ pub fn startup(bench_time: &u64) {
     println!("{BOLD}  {GREEN}* ---------------------------------- *{GREEN}{C_RESET}\n");
 }
 
+pub fn return_result(bench_time: &u64) {
+    match bench_time.to_string().len() {
+        1 => println!("{BOLD}  {GREEN}|{RESET}                  {CYAN}{bench_time}                 {GREEN}|{RESET}{C_RESET}"),
+        2 => println!("{BOLD}  {GREEN}|{RESET}                 {CYAN}{bench_time}                 {GREEN}|{RESET}{C_RESET}"),
+        3 => println!("{BOLD}  {GREEN}|{RESET}                {CYAN}{bench_time}                {GREEN}|{RESET}{C_RESET}"),
+        4 => println!("{BOLD}  {GREEN}|{RESET}                {CYAN}{bench_time}                {GREEN}|{RESET}{C_RESET}"),
+        5 => println!("{BOLD}  {GREEN}|{RESET}                {CYAN}{bench_time}                {GREEN}|{RESET}{C_RESET}"),
+        6 => println!("{BOLD}  {GREEN}|{RESET}                {CYAN}{bench_time}                {GREEN}|{RESET}{C_RESET}"),
+        _ => std::process::exit(1),
+    }
+}
+
+pub fn result(result: u64) {
+    println!("\n{BOLD}  {GREEN}* ----- Your results are here! ----- *{RESET}{C_RESET}");
+    println!("{BOLD}  {GREEN}|{RESET}      Your computer has scored      {GREEN}|{RESET}{C_RESET}");
+    return_result(&result);
+    println!("{BOLD}  {GREEN}|{RESET}              points!               {GREEN}|{RESET}{C_RESET}");
+    println!("{BOLD}  {GREEN}|{RESET}            cpu core!               {GREEN}|{RESET}{C_RESET}");
+    println!("{BOLD}  {GREEN}* ---------------------------------- *{GREEN}{C_RESET}\n");
+}
+
