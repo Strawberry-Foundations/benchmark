da6effe24155587a9138930bd557794e5bfb445e
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-01-01 17:15:48 +0100

    Added Benchmark Struct

---
+++ src/engines/benchmark.rs
@@ -0,0 +1,27 @@
+use crate::type_modes::{CPUMode, Modes};
+
+pub struct Benchmark {
+    pub mode: Modes,
+    pub benchmark_duration: u64,
+    pub num_counter: NumCounter
+}
+
+pub struct NumCounter {
+    pub threading_mode: CPUMode,
+    pub threads: u8,
+    pub counter: bool,
+}
+
+impl Benchmark {
+    pub fn new() -> Self {
+        Self {
+            mode: Modes::NONE,
+            benchmark_duration: 10,
+            num_counter: NumCounter {
+                threading_mode: CPUMode::SINGLE,
+                threads: u8::try_from(std::thread::available_parallelism().unwrap().get()).unwrap(),
+                counter: false,
+            },
+        }
+    }
+}
