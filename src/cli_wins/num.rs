7f1d7042eaa78300c4e2e07133b5443ab2dc3578
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-01-01 23:16:48 +0100

    git 👎

--- src/cli_wins/num.rs
+++ src/cli_wins/num.rs
@@ -4,7 +4,7 @@ use crate::type_modes::CPUMode;
 use crate::VERSION;
 
 use std::thread::available_parallelism;
-use raw_cpuid::CpuId;
+// use raw_cpuid::CpuId;
 
 pub fn print_cpu_mode(cpu_mode: &CPUMode) {
     match cpu_mode {
@@ -21,10 +21,13 @@ pub fn startup(bench_time: u64, cpu_mode: &CPUMode) {
     print_cpu_mode(cpu_mode);
     println!("{BOLD}  {GREEN}* ---------------------------------- *{GREEN}{C_RESET}\n");
 
+    /*
     let cpuid = CpuId::new();
     let binding = cpuid.get_processor_brand_string().unwrap();
     let cpu_string = binding.as_str();
+    
+    println!("{BOLD}{MAGENTA}CPU: {C_RESET}{}", cpu_string);
+    */
 
-    println!("{BOLD}{MAGENTA}CPU: {C_RESET}{cpu_string}");
     println!("{BOLD}{MAGENTA}Logical Processors: {C_RESET}{}", available_parallelism().unwrap().get());
 }
