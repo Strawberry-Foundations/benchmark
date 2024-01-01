0ceb1d421f3dd9aff7454d67cfb96d2faac5a73e
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-01-01 23:37:14 +0100

    moved logical processors text to cli window

--- src/cli_wins/num.rs
+++ src/cli_wins/num.rs
@@ -1,5 +1,6 @@
+use std::num::TryFromIntError;
 use crate::cli_wins::time::return_bench_time;
-use crate::colors::{BOLD, C_RESET, GREEN, MAGENTA, RESET};
+use crate::colors::{BOLD, C_RESET, CYAN, GREEN, MAGENTA, RESET};
 use crate::type_modes::CPUMode;
 use crate::VERSION;
 
@@ -13,9 +14,23 @@ pub fn print_cpu_mode(cpu_mode: &CPUMode) {
     }
 }
 
+pub fn get_logical_processors(cores: Result<u8, TryFromIntError>) {
+    let cores = cores.unwrap().to_string();
+
+    match cores.len() {
+        1 => println!("{BOLD}  {GREEN}|{RESET}        Logical Processors: {CYAN}{cores}       {GREEN}|{RESET}{C_RESET}"),
+        2 => println!("{BOLD}  {GREEN}|{RESET}       Logical Processors: {CYAN}{cores}       {GREEN}|{RESET}{C_RESET}"),
+        3 => println!("{BOLD}  {GREEN}|{RESET}       Logical Processors: {CYAN}{cores}      {GREEN}|{RESET}{C_RESET}"),
+        4 => println!("{BOLD}  {GREEN}|{RESET}      Logical Processors: {CYAN}{cores}      {GREEN}|{RESET}{C_RESET}"),
+        _ => println!("{BOLD}  {GREEN}|{RESET}      Logical Processors: {CYAN}....      {GREEN}|{RESET}{C_RESET}"),
+    }
+
+}
+
 pub fn startup(bench_time: u64, cpu_mode: &CPUMode) {
     println!("\n{BOLD}  {GREEN}* ---- Benchmarking Tool v{VERSION} ---- *{RESET}{C_RESET}");
     return_bench_time(bench_time);
+    get_logical_processors(u8::try_from(available_parallelism().unwrap().get()));
     println!("{BOLD}  {GREEN}|{RESET}                                    {GREEN}|{RESET}{C_RESET}");
     println!("{BOLD}  {GREEN}|{RESET}           Current mode:            {GREEN}|{RESET}{C_RESET}");
     print_cpu_mode(cpu_mode);
@@ -28,6 +43,4 @@ pub fn startup(bench_time: u64, cpu_mode: &CPUMode) {
     
     println!("{BOLD}{MAGENTA}CPU: {C_RESET}{}", cpu_string);
     */
-
-    println!("{BOLD}{MAGENTA}Logical Processors: {C_RESET}{}", available_parallelism().unwrap().get());
 }
