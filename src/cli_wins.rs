27983df4da4af8c97abfeeb17fe71b92e240d4eb
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-01-01 13:33:06 +0100

    Fixed Clippy Warnings

--- src/cli_wins.rs
+++ src/cli_wins.rs
@@ -1,7 +1,7 @@
 use crate::colors::{BOLD, C_RESET, CYAN, GREEN, RED, RESET};
 use crate::VERSION;
 
-pub fn return_bench_time(bench_time: &u64) {
+pub fn return_bench_time(bench_time: u64) {
     match bench_time.to_string().len() {
         1 => println!("{BOLD}  {GREEN}|{RESET}       Set benchmark time: {CYAN}{bench_time}s       {GREEN}|{RESET}{C_RESET}"),
         2 => println!("{BOLD}  {GREEN}|{RESET}       Set benchmark time: {CYAN}{bench_time}s      {GREEN}|{RESET}{C_RESET}"),
@@ -15,7 +15,7 @@ pub fn return_bench_time(bench_time: &u64) {
     }
 }
 
-pub fn startup(bench_time: &u64) {
+pub fn startup(bench_time: u64) {
     println!("\n{BOLD}  {GREEN}* ---- Benchmarking Tool v{VERSION} ---- *{RESET}{C_RESET}");
     return_bench_time(bench_time);
     println!("{BOLD}  {GREEN}|{RESET}    This tool will only use one     {GREEN}|{RESET}{C_RESET}");
@@ -23,7 +23,7 @@ pub fn startup(bench_time: &u64) {
     println!("{BOLD}  {GREEN}* ---------------------------------- *{GREEN}{C_RESET}\n");
 }
 
-pub fn return_result(bench_time: &u64) {
+pub fn return_result(bench_time: u64) {
     match bench_time.to_string().len() {
         1 => println!("{BOLD}  {GREEN}|{RESET}                  {CYAN}{bench_time}                 {GREEN}|{RESET}{C_RESET}"),
         2 => println!("{BOLD}  {GREEN}|{RESET}                 {CYAN}{bench_time}                 {GREEN}|{RESET}{C_RESET}"),
@@ -38,7 +38,7 @@ pub fn return_result(bench_time: &u64) {
 pub fn result(result: u64) {
     println!("\n{BOLD}  {GREEN}* ----- Your results are here! ----- *{RESET}{C_RESET}");
     println!("{BOLD}  {GREEN}|{RESET}      Your computer has scored      {GREEN}|{RESET}{C_RESET}");
-    return_result(&result);
+    return_result(result);
     println!("{BOLD}  {GREEN}|{RESET}               points!              {GREEN}|{RESET}{C_RESET}");
     println!("{BOLD}  {GREEN}* ---------------------------------- *{GREEN}{C_RESET}\n");
 }
