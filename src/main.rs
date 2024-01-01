953ac4cfbc2b958aa5093edb39f4596e06808740
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-01-01 16:10:21 +0100

    complete new arg system

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
-#![allow(dead_code)]
 #![warn(clippy::all, clippy::nursery, clippy::pedantic)]
+#![allow(clippy::match_same_arms, dead_code)]
 
 mod colors;
 mod utilities;
 mod engines;
 mod cli_wins;
 mod cli;
+mod type_modes;
 
 use crate::cli::args::Args;
+use crate::type_modes::{CPUMode, Modes};
 
 pub const VERSION: &str = "1.1.0";
 
 fn main() {
     let mut args = Args::new();
-    let _collector = args.collector();
+    let collector = args.collector();
 
-    let bench_time = args.bench_time;
+    let mut mode: Modes = Modes::NUMBERS;
+    let mut threading_mode: CPUMode = CPUMode::SINGLE;
+    let mut show_counter: bool = false;
 
-    cli_wins::main::startup(bench_time);
+    let mut cpu_threads  = u8::try_from(std::thread::available_parallelism().unwrap().get()).unwrap();
+
+    let mut bench_time: u64 = 10;
+
+
+    for (index, arg) in collector.iter().enumerate() {
+        match arg.as_str() {
+            "-n" | "--numbers" => mode              = Modes::NUMBERS,
+            "-g" | "--graphic" => mode              = Modes::GRAPHIC,
+            "-m" | "--multi"   => threading_mode    = CPUMode::MULTI,
+            "-s" | "--single"  => threading_mode    = CPUMode::SINGLE,
+            "-c" | "--counter" => show_counter      = true,
+            "-h" | "--threads" => cpu_threads       = collector.get(index + 1)
+                                                               .map_or(10,|val| val.parse::<u8>().map_or(10, |cpu_threads| cpu_threads)),
+            _    => {  }
+        };
+
+        if arg.as_str() == "-t" {
+            bench_time = collector.get(index + 1).map_or(10, |val| val.parse::<u64>().map_or(10, |parsed_time| parsed_time));
+        }
+    }
+
+    cli_wins::main::startup(bench_time, &mode);
 
     utilities::start_counter();
 
-    engines::num_counter::single::benchmark(bench_time, false); // might change this later
-    // engines::num_counter::multi::benchmark(16, bench_time);
+    match mode {
+        Modes::NUMBERS => args.bench_num(bench_time, &threading_mode, cpu_threads, show_counter),
+        Modes::GRAPHIC | Modes::NONE => { },
+    }
 }
