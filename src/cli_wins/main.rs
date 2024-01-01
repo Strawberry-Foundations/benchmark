9277fb67f95104ad66fc25b78d905b14a6cd78de
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-01-01 16:10:19 +0100

    complete new arg system

--- src/cli_wins/main.rs
+++ src/cli_wins/main.rs
@@ -1,11 +1,14 @@
 use crate::cli_wins::time::return_bench_time;
 use crate::colors::{BOLD, C_RESET, GREEN, RESET};
+use crate::type_modes::Modes;
 use crate::VERSION;
 
-pub fn startup(bench_time: u64) {
+pub fn startup(bench_time: u64, modes: &Modes) {
     println!("\n{BOLD}  {GREEN}* ---- Benchmarking Tool v{VERSION} ---- *{RESET}{C_RESET}");
     return_bench_time(bench_time);
     println!("{BOLD}  {GREEN}|{RESET}    This tool will only use one     {GREEN}|{RESET}{C_RESET}");
     println!("{BOLD}  {GREEN}|{RESET}              CPU core!             {GREEN}|{RESET}{C_RESET}");
     println!("{BOLD}  {GREEN}* ---------------------------------- *{GREEN}{C_RESET}\n");
+
+    println!("{:?}", modes);
 }
