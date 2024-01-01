4fd09ad8f03a136fe11ed743773c9bfa0788f820
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-01-01 13:47:28 +0100

    Split cli_wins to multiple files

---
+++ src/cli_wins/main.rs
@@ -0,0 +1,11 @@
+use crate::cli_wins::time::return_bench_time;
+use crate::colors::{BOLD, C_RESET, GREEN, RESET};
+use crate::VERSION;
+
+pub fn startup(bench_time: u64) {
+    println!("\n{BOLD}  {GREEN}* ---- Benchmarking Tool v{VERSION} ---- *{RESET}{C_RESET}");
+    return_bench_time(bench_time);
+    println!("{BOLD}  {GREEN}|{RESET}    This tool will only use one     {GREEN}|{RESET}{C_RESET}");
+    println!("{BOLD}  {GREEN}|{RESET}              CPU core!             {GREEN}|{RESET}{C_RESET}");
+    println!("{BOLD}  {GREEN}* ---------------------------------- *{GREEN}{C_RESET}\n");
+}
