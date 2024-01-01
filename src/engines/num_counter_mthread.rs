66eabb34a064400d9b7ef3584765b60438896b89
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-01-01 13:24:25 +0100

    Added Multithreading

---
+++ src/engines/num_counter_mthread.rs
@@ -0,0 +1,49 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+use crate::utilities::delete_last_line;
+
+pub fn main(time: u64) {
+    let running = Arc::new(AtomicBool::new(true));
+    let mut handles = vec![];
+    let mut num_threads: u32 = 16;
+
+    let bench_time = time;
+
+    for _ in 0..num_threads {
+        let running_clone = running.clone();
+        handles.push(thread::spawn(move || {
+            let mut c: u64 = 0;
+
+            while running_clone.load(Ordering::Relaxed) {
+                c += 1;
+            }
+
+            c
+        }));
+    }
+
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(bench_time));
+        running.store(false, Ordering::Relaxed);
+    });
+
+    let mut results = Vec::new();
+
+    for handle in handles {
+        results.push(handle.join().unwrap());
+    }
+
+    let total_count: u64 = results.iter().sum();
+    let bench_time_ms = bench_time * 1000;
+    let result = (total_count + bench_time_ms) / 900000;
+
+    delete_last_line();
+    thread::sleep(Duration::from_secs(1));
+    println!("Finished Benchmark");
+
+    crate::cli_wins::result(result);
+
+    // println!("{}", total_count);
+}
