7157dfb67f88314b5aa8dd5d0b32fd619902876f
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-01-01 16:39:45 +0100

    Added help section

--- src/cli/sections.rs
+++ src/cli/sections.rs
@@ -1,6 +1,18 @@
-use crate::colors::{C_RESET, GREEN, BOLD};
+use crate::colors::{C_RESET, GREEN, BOLD, UNDERLINE, CYAN, RESET, WHITE, RED, MAGENTA};
+use crate::VERSION;
 
 pub fn help_section() {
-    println!("{BOLD}{GREEN}Benchmarking Tool v1.1.0{C_RESET}");
+    println!("\
+{BOLD}{CYAN}{UNDERLINE}Benchmarking Tool v{VERSION}{C_RESET}\n\
+{GREEN}{BOLD}Usage:{RESET} {WHITE}benchmark {CYAN}[command] {RED}[<options>]{C_RESET}\n\n\
+{MAGENTA}{BOLD}Modes:{C_RESET}
+    {CYAN}{BOLD}-n, --numbers:{C_RESET} Number Benchmark Engine {GREEN}{BOLD}[default]{C_RESET}
+     {BOLD}↳ {MAGENTA}Options:{C_RESET}
+            {CYAN}{BOLD}-s, --single{C_RESET}       Run Number Benchmark Single-threaded {GREEN}{BOLD}[default]{C_RESET}
+            {CYAN}{BOLD}-m, --multi{C_RESET}        Run Number Benchmark Multi-threaded
+            {CYAN}{BOLD}-h, --threads  <n>{C_RESET} Specify the number of threads used for multithreading
+            {CYAN}{BOLD}-c, --counter{C_RESET}      Show number counter
+
+");
     std::process::exit(0);
 }
